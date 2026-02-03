a09501e4a367fd299ba46165ece0ee9176336350
Jair Morales <jimafotos247@gmail.com>
2026-02-03 16:06:52 -0600

    Update Controller.java

--- src/Controller.java
+++ src/Controller.java
@@ -1,1 +1,11 @@
-
+import java.io.PrintStream;
+import java.util.Scanner;
+import java.util.Vector;
+public class Controller {
+    private final PrintStream imp;
+    private final Scanner sc;
+    private PostfiProcessor calculator;
+    private FilesManagment fileControl;
+    private String dataTarget;
+    private String resultTarget;
+}
